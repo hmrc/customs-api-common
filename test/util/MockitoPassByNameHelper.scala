7e63878bc6a039bc4dbb7df87ae8853f46333945
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2018-04-23 13:02:48 +0100

    CDD-1384: Decoupled PassByNameVerifier from old logging API by making it more general.

--- test/util/MockitoPassByNameHelper.scala
+++ test/util/MockitoPassByNameHelper.scala
@@ -16,21 +16,23 @@
 
 package util
 
-import org.mockito.Mockito
+import org.mockito.{ArgumentMatcher, ArgumentMatchers, Mockito}
 import uk.gov.hmrc.http.HeaderCarrier
 
 object MockitoPassByNameHelper {
 
   sealed trait Builder {
+    def withByNameParamMatcher[P](matcher: => P): Builder
+    def withParamMatcher[P](matcher: => P)(implicit mp: Manifest[P]): Builder
+    def withByNameParam[P](expected: => P): Builder
 
-    def withByNameParam[P](expected: P): Builder
-
+    @deprecated("use the more general withParamMatcher(any[HeaderCarrier]) instead", "1.24.0")
     def withAnyHeaderCarrierParam(): Builder
 
     def verify(): Unit
   }
 
-  case class PassByNameParam(clazz: Class[_], paramMatcher: AnyRef)
+  case class PassByNameParam(clazz: Class[_], paramMatcher: Any)
 
   /**
     * Work around for the fact that Mockito can not handle Scala pass by name parameters
@@ -42,34 +44,52 @@ object MockitoPassByNameHelper {
     *  PassByNameVerifier(mockDeclarationLogger, "error")
     *    .withByNameParam[String](s"Call to get api subscription fields failed. url=$expectedUrl")
     *    .withByNameParam[Throwable](caught)
-    *    .withAnyHeaderCarrierParam
     *    .verify()
     * }}}
-    *
+    * There is also the ability to apply Mockito ArgumentMatchers to both normal and pass by name parameters:
+    * {{{
+    *  PassByNameVerifier(mockFoo, "someCall")
+    *    .withParamMatcher(any[String])
+    *    .withParamMatcher(ameq[String]("some value"))
+    *    .withByNameParamMatcher(any[() => Unit])
+    *    .verify()
+    * }}}
     */
-  case class PassByNameVerifier[T](mockedInstance: T, methodName: String, params: Seq[PassByNameParam] = Seq.empty)(implicit m: Manifest[T]) extends Builder {
+  case class PassByNameVerifier[T](mockedInstance: T, methodName: String, params: Seq[PassByNameParam] = Seq.empty, maybeOngoingVerification: Option[T] = None) extends Builder {
 
-    def withByNameParam[P](expected: P): Builder = this.copy(params = this.params :+ PassByNameParam(classOf[() => P], genericPassByName[P](expected)))
+    private def initOngoingVerification: PassByNameVerifier[T] =
+      if (maybeOngoingVerification.isEmpty) {
+        this.copy(maybeOngoingVerification = Some(Mockito.verify(mockedInstance)))
+      } else {
+        this
+      }
 
-    def withAnyHeaderCarrierParam(): Builder = this.copy(params = this.params :+ PassByNameParam(classOf[HeaderCarrier], anyHeaderCarrierMatcher))
+    def withByNameParamMatcher[P](matcher: => P): Builder = {
+      initOngoingVerification.copy(params = this.params :+ PassByNameParam(classOf[() => P], matcher))
+    }
+
+    def withParamMatcher[P](matcher: => P)(implicit mp: Manifest[P]): Builder = {
+      val c = mp.runtimeClass.asInstanceOf[Class[P]]
+      initOngoingVerification.copy(params = this.params :+ PassByNameParam(c, matcher))
+    }
+
+    def withByNameParam[P](expected: => P): Builder = {
+      initOngoingVerification.copy(params = this.params :+ PassByNameParam(classOf[() => P], ArgumentMatchers.argThat(byNameEqArgMatcher(() => expected))))
+    }
+
+    def withAnyHeaderCarrierParam(): Builder = {
+      withParamMatcher(ArgumentMatchers.any[HeaderCarrier])
+    }
 
     def verify(): Unit = {
       require(params.nonEmpty, "no parameters specified.")
-      val method = m.runtimeClass.asInstanceOf[Class[T]].getMethod(methodName, params.map(param => param.clazz): _*)
-      method.invoke(
-        Mockito.verify(mockedInstance),
-        params.map(param => param.paramMatcher): _*
-      )
+      val method = mockedInstance.getClass.getMethod(methodName, params.map(param => param.clazz): _*)
+      method.invoke(maybeOngoingVerification.get, params.map(param => param.paramMatcher.asInstanceOf[AnyRef]): _*)
     }
 
-    private lazy val anyHeaderCarrierMatcher = new HeaderCarrier() {
-      override def equals(o: Any): Boolean = o.getClass == classOf[HeaderCarrier]
-      override def hashCode(): Int = super.hashCode()
-    }
-
-    private def genericPassByName[P](expected: P): () => P = new (() => P) {
-      override def equals(o: Any): Boolean = expected == o.asInstanceOf[() => P].apply()
-      def apply(): P = throw new IllegalArgumentException("unexpected apply()")
+    private def byNameEqArgMatcher[P](compare: P) = new ArgumentMatcher[P] {
+      override def matches(argument: P): Boolean =
+        argument.asInstanceOf[() => P].apply() == compare.asInstanceOf[() => P].apply()
     }
   }
 }
