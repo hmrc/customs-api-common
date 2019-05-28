dacbdba755c04af5a73db5443b21bb248d04f595
Rashad Mughal <rashadmughal@users.noreply.github.com>
2019-05-28 14:38:01 +0100

    CDD-2266: Remove service-locator. (#23)

--- app/uk/gov/hmrc/customs/api/common/CustomsApiCommonModule.scala
+++ app/uk/gov/hmrc/customs/api/common/CustomsApiCommonModule.scala
@@ -18,13 +18,11 @@ package uk.gov.hmrc.customs.api.common
 
 import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.customs.api.common.services.ServiceLocatorRegistrationService
 import uk.gov.hmrc.play.bootstrap.http.{DefaultHttpClient, HttpClient}
 
 class CustomsApiCommonModule extends Module {
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = Seq(
-    bind[ServiceLocatorRegistrationService].toSelf.eagerly(),
     bind[HttpClient].to[DefaultHttpClient]
   )
 
