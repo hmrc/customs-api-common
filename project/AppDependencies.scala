01ba9964783002568bb10c6665d567a2b8aa8d3b
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2019-07-18 16:10:57 +0100

    CDD-1882: minor tidy-up

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlay25Version = "4.13.0"
   private val bootstrapPlay26Version = "0.42.0"
   private val xmlResolverVersion = "1.2"
   private val catsVersion = "1.6.1"
-  private val hmrcTestVersion = "3.9.0-play-25"
+  private val hmrcTestVersion = "3.9.0-play-26"
   private val scalaTestVersion = "3.0.8"
   private val scalatestplusVersion = "3.1.2"
   private val wireMockVersion = "2.23.2"
   private val mockitoVersion = "3.0.0"
   private val testScope = "test,it"
 
-  val bootstrapPlay25 = "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrapPlay25Version
   val bootstrapPlay26 = "uk.gov.hmrc" %% "bootstrap-play-26" % bootstrapPlay26Version
   
   val xmlResolver = "xml-resolver" % "xml-resolver" % xmlResolverVersion
